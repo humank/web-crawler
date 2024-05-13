afe49f577853664967fec15b7c5cd71a7ba0e16b
Kim Kao <yikaikao@amazon.com>
2024-05-13 14:43:07 +0800

    upload all of the initialize code.

--- src/main/java/com/amazonaws/devtx/webcrawler/configuration/WebCrawlerConfiguration.java
+++ src/main/java/com/amazonaws/devtx/webcrawler/configuration/WebCrawlerConfiguration.java
@@ -1,2 +1,29 @@
-package com.amazonaws.devtx.webcrawler.configuration;public class WebCrawlerConfiguration {
+package com.amazonaws.devtx.webcrawler.configuration;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.devtx.webcrawler.application.eventhandlers.WebsiteEventHandler;
+import com.amazonaws.devtx.webcrawler.domain.services.WebCrawlerServiceImpl;
+import com.amazonaws.devtx.webcrawler.infrastructure.persistence.WebsiteRepositoryImpl;
+
+@Configuration
+public class WebCrawlerConfiguration {
+
+    @Bean
+    public WebsiteRepositoryImpl websiteRepository() {
+        return new WebsiteRepositoryImpl();
+    }
+
+    @Bean
+    public WebsiteEventHandler websiteEventHandler() {
+        return new WebsiteEventHandler();
+    }
+
+    @Bean
+    public WebCrawlerServiceImpl webCrawlerService(WebsiteRepositoryImpl websiteRepository, ApplicationEventPublisher eventPublisher) {
+        return new WebCrawlerServiceImpl(websiteRepository, eventPublisher);
+    }
+
 }
